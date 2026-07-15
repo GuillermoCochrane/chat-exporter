d9df4e2debc10cbbff675045c155d2691e3331ad
Guille <cochraneguillermo@gmail.com>
2026-07-15 20:22:35 -0300

    feat(cli): agregar comando de ayuda en CLI

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,36 @@
-export function parseArguments(argv = process.argv) {
-  const [, , input, output] = argv;
+function showHelp() {
+  console.log(`
+Chat Exporter v0.5.8
+
+Uso:
+
+  npm start
+  npm start -- <input>
+  npm start -- <input> <output>
+
+Ejemplos:
+
+  npm start
+  npm start -- input/epistolario_MINI.json
+  npm start -- input/epistolario_SMALL.json output/prueba.md
+
+Opciones:
+
+  -h, --help       Muestra esta ayuda.
+  -v, --version    Muestra la versión.
+`);
+}
+
+export function parseArguments() {
+  const args = process.argv.slice(2);
+
+  if (args.includes("-h") || args.includes("--help")) {
+    showHelp();
+    process.exit(0);
+  }
 
   return {
-    input: input ?? "./input/epistolario_SMALL.json",
-    output: output ?? "./output/conversacion.md",
+    input: args[0] ?? "./input/epistolario_SMALL.json",
+    output: args[1] ?? "./output/conversacion.md",
   };
 }
