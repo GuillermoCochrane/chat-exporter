56d125a9648503de46d6c05478d65df02f0850fb
Guille <cochraneguillermo@gmail.com>
2026-07-16 17:00:18 -0300

    feat(cli): incorporar validación de argumentos obligatorios

--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,27 @@
-// Valid actions for the command-line arguments
+// validator.js
+
+const validatorMessages = {
+  unknownOption: (option) => ` Opción desconocida: ${option}`,
+
+  missingValue: (option) => ` La opción "${option}" requiere un valor.`,
+};
+
+function throwValidationError(type, value) {
+  throw new Error(validatorMessages[type](value));
+}
+
 export function validateArguments(args, actions) {
-  for (const arg of args) {
+  //Validaciones del CLI
+
+  // Opciones validas
+  for (const [index, arg] of args.entries()) {
     if (!arg.startsWith("-")) continue;
-    if (!(arg in actions)) throw new Error(` Opción desconocida: ${arg}`);
+
+    if (!(arg in actions)) throwValidationError("unknownOption", arg);
+    
+    const action = actions[arg];
+    
+    // Parametros requeridos para la acción
+    if (action.consumes > 0 && args[index + action.consumes] === undefined) throwValidationError("missingValue", arg);
   }
 }
