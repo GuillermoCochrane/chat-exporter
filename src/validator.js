02f2cadbc39e8ac0a00bc7e287612e4eb155084a
Guille <cochraneguillermo@gmail.com>
2026-07-17 01:28:22 -0300

    feat: validar extensiones de archivos en la CLI

--- src/validator.js
+++ src/validator.js
@@ -4,25 +4,32 @@ const validatorMessages = {
   unknownOption: (option) => ` Opción desconocida: ${option}`,
 
   missingValue: (option) => ` La opción "${option}" requiere un valor.`,
+
+  invalidExtension: (file, type) => ` El archivo de ${type === "input" ? "entrada" : "salida"} "${file}" debe tener extensión "${type === "input" ? ".json" : ".md"}".`,
 };
 
-function throwValidationError(type, value) {
-  throw new Error(validatorMessages[type](value));
+// Lanza un Error utilizando el mensaje correspondiente.
+function throwValidationError(type, ...args) {
+  throw new Error(validatorMessages[type](...args));
 }
 
 // Validaciones de la interfaz de línea de comandos.
 export function validateArguments(args, actions) {
-  // Opciones validas
   for (const [index, arg] of args.entries()) {
     if (!arg.startsWith("-")) continue;
 
+    // Opciones validas
     if (!(arg in actions)) throwValidationError("unknownOption", arg);
+    
 
     const action = actions[arg];
-
-    // Parámetros requeridos para la acción.
     const value = args[index + action.consumes];
 
-    if (action.consumes > 0 && (value === undefined || value.startsWith("-"))) { throwValidationError("missingValue", arg); }
+    // Parámetros requeridos para la acción.
+    if (action.consumes > 0 && (value === undefined || value.startsWith("-")))  throwValidationError("missingValue", arg);
+
+    // Extensión esperada.
+    if ((arg === "-i" || arg === "--input") && !value.endsWith(".json") ) throwValidationError("invalidExtension", value, "input");
+    if ((arg === "-o" || arg === "--output") && !value.endsWith(".md")) throwValidationError("invalidExtension", value, "output");
   }
 }
