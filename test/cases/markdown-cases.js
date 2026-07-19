cb0643212f6ea59066b665d0f55dedb3fe79bd58
Guille <cochraneguillermo@gmail.com>
2026-07-19 01:11:45 -0300

    test: agregar casos de prueba para markdown

---
+++ test/cases/markdown-cases.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    role: "user",
+    text: "Hola\n\n¿Cómo va todo?",
+    timestamp: 1750582900,
+  },
+  {
+    role: "assistant",
+    text: "Muy bien.",
+    timestamp: 1750582910,
+  },
+];
