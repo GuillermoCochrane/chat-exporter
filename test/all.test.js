0cd130d5a731bc3afa3561bf4ea7c5b0f3d1ad87
Guille <cochraneguillermo@gmail.com>
2026-07-19 01:16:40 -0300

    test: agregar runner para ejecutar la suite completa

---
+++ test/all.test.js
@@ -0,0 +1,10 @@
+import "./formatter.test.js";
+import "./validator.test.js";
+import "./parser.test.js";
+import "./filter.test.js";
+import "./normalizer.test.js";
+import "./markdown.test.js";
+import "./markdown-compact.test.js";
+
+console.log("\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+console.log("✔ Todas las pruebas finalizaron correctamente.");
