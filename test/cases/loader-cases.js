300de7574eee7befb0b472b031fd5ce4c767ab93
Guille <cochraneguillermo@gmail.com>
2026-07-19 22:49:35 -0300

    test: agregar casos de prueba para loader

---
+++ test/cases/loader-cases.js
@@ -0,0 +1,6 @@
+export default {
+  MINI: "./test/fixtures/test_data_MINI.json",
+  SMALL: "./test/fixtures/test_data_SMALL.json",
+  INVALID: "./test/fixtures/test_data_INVALID.json",
+  MISSING: "./test/fixtures/no_existe.json",
+};
