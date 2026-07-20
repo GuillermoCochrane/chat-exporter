0d08b8312476227c691cf267b6c4700eddb217c6
Guille <cochraneguillermo@gmail.com>
2026-07-20 04:48:12 -0300

    refactor: documentar módulo wirter

--- src/writer.js
+++ src/writer.js
@@ -1,5 +1,6 @@
 import { writeFile } from "node:fs/promises";
 
+// Escribe el contenido generado en un archivo utilizando UTF-8.
 export async function writeFileContent(path, content) {
   await writeFile(path, content, "utf8");
 }
