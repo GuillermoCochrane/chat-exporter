564fc1f20b5c5c0cc0acd138e380dc0a5d775849
Guille <cochraneguillermo@gmail.com>
2026-08-07 18:24:15 -0300

    feat(pages): agregar helpers de manipulación del DOM

---
+++ assets/js/utilities/dom.js
@@ -0,0 +1,22 @@
+//? Funciones para manipular el DOM
+
+//* Obtener elemento DOM
+export const $ = (selector) => document.querySelector(selector);
+
+//* Obtener todos los elementos DOM
+export const $$ = (selector) => document.querySelectorAll(selector);
+
+//* Cambiar el texto de un elemento DOM
+export const setText = (selector, text) => { ($(selector)) && ($(selector).textContent = text); };
+
+//* Crea elemento DOM con su clase
+export function createElement(tagName, className = null, content = null, isHTML = false, id = null) {
+    const element = document.createElement(tagName);
+    className && (element.className = className);
+    id && (element.id = id);
+    if (content !== null) {
+        isHTML ? (element.innerHTML = content) : (element.textContent = content);
+    }
+
+    return element;
+}
