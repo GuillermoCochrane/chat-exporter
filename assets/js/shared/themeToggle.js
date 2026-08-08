1cd563f6f672d61eeefa7df7b9d159518840cf87
Guille <cochraneguillermo@gmail.com>
2026-08-08 19:44:48 -0300

    feat(pages): agregar persistencia y estado visual al toggle de tema

--- assets/js/shared/themeToggle.js
+++ assets/js/shared/themeToggle.js
@@ -1,5 +1,21 @@
 //* Theme toggle
+import { $ , setText } from "../utilities/dom.js"
 
 export function themeToggle() {
-  document.getElementById('themeToggle').addEventListener('click', () => document.body.classList.toggle('light'));
+  // ---------- THEME TOGGLE ----------
+  const $themeToggle = $('#themeToggle');
+  const $html = document.documentElement;
+
+  const currentTheme = localStorage.getItem('theme') || 'dark';
+  const themeIcon = currentTheme === 'dark' ? '☀️' : '🌙';
+
+  $html.setAttribute('data-theme', currentTheme);
+  setText("#themeToggle", themeIcon);
+
+  $themeToggle.addEventListener('click', () => {
+      const newTheme = $html.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+      $html.setAttribute('data-theme', newTheme);
+      localStorage.setItem('theme', newTheme);
+      setText("#themeToggle", newTheme === 'dark' ? '☀️' : '🌙');
+  });
 }
