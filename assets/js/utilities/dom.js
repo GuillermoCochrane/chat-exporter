d7a283dee7937437c5a6156896e6c44571916436
Guille <cochraneguillermo@gmail.com>
2026-08-13 19:49:06 -0300

    feat(pages): agregar setValue para manipulación genérica del DOM

--- assets/js/utilities/dom.js
+++ assets/js/utilities/dom.js
@@ -9,6 +9,9 @@ export const $$ = (selector) => document.querySelectorAll(selector);
 //* Cambiar el texto de un elemento DOM
 export const setText = (selector, text) => { ($(selector)) && ($(selector).textContent = text); };
 
+//* Cambiar el valor de un elemento DOM
+export const setValue = (selector, propietary, value) => { ($(selector)) && ($(selector)[propietary] = value); };
+
 //* Crea elemento DOM con su clase
 export function createElement(tagName, className = null, content = null, isHTML = false, id = null) {
     const element = document.createElement(tagName);
