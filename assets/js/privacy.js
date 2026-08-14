73bb1f39b83d23f5d541189db6b2fdd3dfdc4d5b
Guille <cochraneguillermo@gmail.com>
2026-08-14 08:00:46 -0300

    feat(pages): integrar sistema multiidioma en la página de privacidad para secciones comunes

---
+++ assets/js/privacy.js
@@ -0,0 +1,10 @@
+//*  Privacy Page orchestrator
+
+import { initCommon } from './shared/commonHandler.js';
+import { languageHandler } from './shared/languageHandler.js';
+import { COMMONTRANSLATIONS } from './languages/common.js';
+
+const TRANSLATIONS = { ...COMMONTRANSLATIONS };
+
+initCommon();
+languageHandler(TRANSLATIONS);
