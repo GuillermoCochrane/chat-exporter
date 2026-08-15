3b0a6ccf0d6a5f29fdaf6432104ae5152c76ef83
Guille <cochraneguillermo@gmail.com>
2026-08-15 19:31:25 -0300

    feat(pages): integrar traducciones de CLI en HTML y orquestador

--- assets/js/cli.js
+++ assets/js/cli.js
@@ -3,8 +3,9 @@
 import { initCommon } from './shared/commonHandler.js';
 import { languageHandler } from './shared/languageHandler.js';
 import { COMMONTRANSLATIONS } from './languages/common.js';
+import { CLITRANSLATIONS } from './languages/cli.js';
 
-const TRANSLATIONS = { ...COMMONTRANSLATIONS,  };
+const TRANSLATIONS = { ...COMMONTRANSLATIONS, ...CLITRANSLATIONS };
 
 initCommon();
 languageHandler(TRANSLATIONS);
