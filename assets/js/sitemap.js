b5f742bee81087d1071475d2ff398731177ab6a7
Guille <cochraneguillermo@gmail.com>
2026-08-16 18:38:10 -0300

    feat(pages): integrar traducciones de sitemap en HTML y orquestador

--- assets/js/sitemap.js
+++ assets/js/sitemap.js
@@ -3,8 +3,9 @@
 import { initCommon } from './shared/commonHandler.js';
 import { languageHandler } from './shared/languageHandler.js';
 import { COMMONTRANSLATIONS } from './languages/common.js';
+import { SITEMAPTRANSLATIONS } from './languages/sitemap.js';
 
-const TRANSLATIONS = { ...COMMONTRANSLATIONS };
+const TRANSLATIONS = { ...COMMONTRANSLATIONS, ...SITEMAPTRANSLATIONS };
 
 initCommon();
 languageHandler(TRANSLATIONS);
