787d709e93f22dce7e01a8b462d139fd1446c95d
Guille <cochraneguillermo@gmail.com>
2026-08-15 21:12:58 -0300

    feat(pages): integrar traducciones de changelog en HTML y orquestador

--- assets/js/changelog.js
+++ assets/js/changelog.js
@@ -3,8 +3,9 @@
 import { initCommon } from './shared/commonHandler.js';
 import { languageHandler } from './shared/languageHandler.js';
 import { COMMONTRANSLATIONS } from './languages/common.js';
+import { CHANGELOGTRANSLATIONS } from './languages/changelog.js';
 
-const TRANSLATIONS = { ...COMMONTRANSLATIONS, };
+const TRANSLATIONS = { ...COMMONTRANSLATIONS, ...CHANGELOGTRANSLATIONS };
 
 initCommon();
 languageHandler(TRANSLATIONS);
