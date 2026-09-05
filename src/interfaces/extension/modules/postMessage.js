ad6bb3c714d70fc31305fa48e8cc6d5ee8a78646
Guille <cochraneguillermo@gmail.com>
2026-09-05 18:58:29 -0300

    fix(extension): corregir import de constante en postMessage

--- src/interfaces/extension/modules/postMessage.js
+++ src/interfaces/extension/modules/postMessage.js
@@ -1,6 +1,6 @@
-import { POST_MESSAGE_ORIGIN } from "./constants.js";
+import { ORIGIN } from "./constants.js";
 
 // Envía un mensaje a la página mediante postMessage.
 export function postMsg(message) {
-  window.postMessage(message, POST_MESSAGE_ORIGIN);
+  window.postMessage(message, ORIGIN);
 }
